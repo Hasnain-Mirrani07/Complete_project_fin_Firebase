a83b5a4316265976eb44300335a584526bad8851
Hasnain-Mirrani07 <hmirrani3@gmail.com>
2023-01-31 11:08:45 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demmo',
+      title: 'Flutter Dmmo',
       theme: ThemeData(
         //thdfofdjf;lkdjflhdfghdfhdgfhs
         primarySwatch: Colors.blue,
