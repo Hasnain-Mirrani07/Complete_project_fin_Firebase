12763b35e6e186c835875fd7a053976056b78168
Hasnain-Mirrani07 <122273493+Hasnain-Mirrani-Tecrux@users.noreply.github.com>
2023-02-19 00:14:53 +0500

    .

--- lib/ui/auth/signup/signup_screen.dart
+++ lib/ui/auth/signup/signup_screen.dart
@@ -31,7 +31,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     alignment: Alignment.center,
                     padding: const EdgeInsets.all(10),
                     child: const Text(
-                      'Tutialart',
+                      'Tuttialart',
                       style: TextStyle(
                           color: Colors.blue,
                           fontWeight: FontWeight.w500,
