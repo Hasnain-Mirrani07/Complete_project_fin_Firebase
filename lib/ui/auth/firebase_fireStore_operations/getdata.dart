58ccd00d0cecd9f91c4f6bc8658a9bc9d8d06a4e
Hasnain-Mirrani07 <hmirrani3@gmail.com>
2023-05-08 21:48:59 +0500

    test

--- lib/ui/auth/firebase_fireStore_operations/getdata.dart
+++ lib/ui/auth/firebase_fireStore_operations/getdata.dart
@@ -42,7 +42,7 @@ class _FireStoreGetDataState extends State<FireStoreGetData> {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const CircularProgressIndicator();
               } else if (snapshot.hasError) {
-                return const Text('FireStoree fddrjgdg DAta Load Errorr');
+                return const Text('FireStoree frjgdg DAta Load Ergrorr');
               } else {
                 return Expanded(
                     child: ListView.builder(
