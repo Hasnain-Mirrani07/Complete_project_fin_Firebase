deee9bf5ae1e7acffbf1c51637700d4fd0aa0509
Hasnain-Mirrani07 <hmirrani3@gmail.com>
2023-05-17 23:49:56 +0500

    .

--- lib/ui/auth/signup/signup_screen.dart
+++ lib/ui/auth/signup/signup_screen.dart
@@ -41,7 +41,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     alignment: Alignment.center,
                     padding: const EdgeInsets.all(10),
                     child: const Text(
-                      'Sing p',
+                      'Singup',
                       style: TextStyle(fontSize: 20),
                     )),
                 Container(
